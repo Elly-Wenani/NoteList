e0f5310bc74da12a674831823fe07634e0a9aecb
Elly Wenani <ellywenani@gmail.com>
2020-06-28 04:53:22 +0300

    Coded the settings

--- app/src/main/java/com/example/notelist/MainActivity.java
+++ app/src/main/java/com/example/notelist/MainActivity.java
@@ -103,8 +103,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings){
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     //Implemented navigation view listener
