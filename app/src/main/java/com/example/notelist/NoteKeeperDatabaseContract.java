0090ccdd7b5ef46d7f4f003385f79a4ed6a0ccbc
Elly Wenani <ellywenani@gmail.com>
2020-07-08 18:49:26 +0300

    Added SqlOpneHelper class NoteKeeperOpenHelper

--- app/src/main/java/com/example/notelist/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/example/notelist/NoteKeeperDatabaseContract.java
@@ -2,10 +2,11 @@ package com.example.notelist;
 
 import android.provider.BaseColumns;
 
-public final class NoteKeeperDatabaseContract{
-    private NoteKeeperDatabaseContract(){} //make non-creatable
+public final class NoteKeeperDatabaseContract {
+    private NoteKeeperDatabaseContract() {
+    } //make non-creatable
 
-    public static final class CourseInfoEntry implements BaseColumns{
+    public static final class CourseInfoEntry implements BaseColumns {
         public static final String TABLE_NAME = "course_info";
         public static final String COLUMN_COURSE_ID = "course_id";
         public static final String COLUMN_COURSE_TITLE = "course_title";
@@ -18,7 +19,7 @@ public final class NoteKeeperDatabaseContract{
                         COLUMN_COURSE_TITLE + " TEXT NOT NULL)";
     }
 
-    public static final class NoteInfoEntry implements BaseColumns{
+    public static final class NoteInfoEntry implements BaseColumns {
         public static final String TABLE_NAME = "note_info";
         public static final String COLUMN_NOTE_TITLE = "note_title";
         public static final String COLUMN_NOTE_TEXT = "note_text";
@@ -26,7 +27,7 @@ public final class NoteKeeperDatabaseContract{
 
         public static final String SQL_CREATE_TABLE =
                 "CREATE_TABLE " + TABLE_NAME + " (" +
-                        _ID + " INTEGER PRIMARY KEY " +
+                        _ID + " INTEGER PRIMARY KEY, " +
                         COLUMN_NOTE_TITLE + " TEXT NOT NULL, " +
                         COLUMN_NOTE_TEXT + " TEXT, " +
                         COLUMN_COURSE_ID + " TEXT NOT NULL)";
